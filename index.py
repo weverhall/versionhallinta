94a633b101d7e54c31a0883408a8f07d3ff7765c
w <weverhall@gmail.com>
2022-11-09 20:27:50 +0200

    main index.py

--- index.py
+++ index.py
@@ -4,11 +4,11 @@ from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
+logger("aloitetaan ohjelma") # muutos masterissa
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
 
-logger("lopetetaan")
+logger("lopetetaan ohjelma")
